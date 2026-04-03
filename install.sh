571fe0cec998af90a4db8b0c4ff1df121e3d0295
XDID <1036443403@qq.com>
2026-04-03 20:03:38 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+ln -sf $(realpath ./bash/bash_profile) /home/harunato/.bash_profile
+ln -sf $(realpath ./bash/bashrc) /home/harunato/.bashrc
+ln -sf $(realpath ./tmux/tumx.conf) /home/harunato/.tmux.conf
+ln -sf $(realpath ./okular/okularpartrc) /home/harunato/.config/okularpartrc
+ln -sf $(realpath ./okular/okularrc) /home/harunato/.config/okularrc
