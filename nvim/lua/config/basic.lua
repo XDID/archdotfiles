fca8dc62aa1ac5f63428d4f0332afeb73aaed588
XDID <1036443403@qq.com>
2026-04-17 23:00:58 +0800

    .

--- nvim/lua/config/basic.lua
+++ nvim/lua/config/basic.lua
@@ -56,9 +56,11 @@ vim.o.confirm = true
 
 -- using space as indent
 vim.o.expandtab = true
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
-vim.o.tabstop = 2
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.tabstop = 4
+vim.o.autoindent = true
+vim.o.smartindent = true
 
 -- nvim win border
 vim.o.winborder = "single"
