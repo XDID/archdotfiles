786265500691c9a53be995962a29d1752f9232d8
XDID <1036443403@qq.com>
2026-04-21 20:33:11 +0800

    .

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,5 +1,5 @@
-require("config.keymaps")
 require("config.lazy")
+require("config.keymaps")
 require("config.basic")
 require("config.fold")
 require("config.autocmds")
---
+++ nvim/lua/plugins/color.lua
@@ -0,0 +1,7 @@
+vim.opt.termguicolors = true
+return {
+	{
+		"norcalli/nvim-colorizer.lua",
+		opts = {},
+	},
+}
