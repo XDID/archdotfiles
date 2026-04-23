9dabb16253f0f50ec2dde352a929dadcb7a39c6a
XDID <1036443403@qq.com>
2026-04-23 12:58:00 +0800

    .

--- fish/alias.sh
+++ fish/alias.sh
@@ -82,6 +82,8 @@ alias ss="sudo systemctl"
 
 alias r="realesrgan-ncnn-vulkan"
 
+alias cal="calcurse"
+
 alias df="df -h -x tmpfs -x efivarfs | awk '!/^dev/' | sort"
 alias free="free -h"
 alias clean="sudo sync;sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'"
