1544e908b21a15b466c789a2a39fd4cfa966e54c
XDID <1036443403@qq.com>
2026-05-03 01:12:33 +0800

    .

--- install.sh
+++ install.sh
@@ -4,3 +4,11 @@
 ln -sf $(realpath ./bash/bash_profile) /home/harunato/.bash_profile
 ln -sf $(realpath ./bash/bashrc) /home/harunato/.bashrc
 ln -sf $(realpath ./tmux/tumx.conf) /home/harunato/.tmux.conf
+
+# config common lisp
+curl -o /tmp/ql.lisp http://beta.quicklisp.org/quicklisp.lisp
+sbcl --no-sysinit --no-userinit --load /tmp/ql.lisp \
+     --eval '(quicklisp-quickstart:install :path "~/.quicklisp")' \
+     --eval '(ql:add-to-init-file)' \
+     --quit
+sbcl --eval '(ql:quickload :quicklisp-slime-helper)' --quit
