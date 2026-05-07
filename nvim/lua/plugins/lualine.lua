b88eeda83803d856bde921295e7c38f29af17868
XDID <1036443403@qq.com>
2026-05-07 21:11:37 +0800

    .

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,9 +1,23 @@
+local function bracket(component)
+	return {
+		component,
+		padding = { left = 0, right = 0 },
+		fmt = function(str)
+			if str == nil or str == "" then
+				return ""
+			end
+
+			return "%#lualine_c_normal#[%s" .. str .. "%#lualine_c_normal#]%s"
+		end,
+	}
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	opts = {
 		options = {
 			theme = "kanagawa",
-			component_separators = { left = "|", right = "|" },
+			component_separators = { left = "", right = "" },
 			section_separators = "",
 			icons_enabled = false,
 		},
@@ -11,17 +25,26 @@ return {
 			lualine_a = {},
 			lualine_b = {},
 			lualine_c = {
-				"mode",
-				"filename",
-				"branch",
-				"diff",
-				"diagnostics",
+				{
+					"filename",
+					path = 3,
+					shorting_target = 0,
+					padding = { left = 0, right = 0 },
+				},
+				bracket("branch"),
+				bracket("diff"),
+				bracket("diagnostics"),
 			},
 			lualine_x = {
-				"overseer",
+				bracket("overseer"),
 				"searchcount",
-				"location",
-				"progress",
+				{
+					function()
+						local current = vim.fn.line(".")
+						local total = vim.fn.line("$")
+						return string.format("[%d/%d]", current, total)
+					end,
+				},
 			},
 			lualine_y = {},
 			lualine_z = {},
